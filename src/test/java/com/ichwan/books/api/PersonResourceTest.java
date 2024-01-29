5a3ebe4966c48efb79b1f45c729691e99a025652
ichwansh03 <ichwansholihin03@gmail.com>
2024-01-29 17:41:18 +0700

    breaking change: add get and get all test

--- src/test/java/com/ichwan/books/api/PersonResourceTest.java
+++ src/test/java/com/ichwan/books/api/PersonResourceTest.java
@@ -2,37 +2,50 @@ package com.ichwan.books.api;
 
 import com.ichwan.api.dto.PersonRequest;
 import com.ichwan.api.resource.PersonResource;
+import com.ichwan.api.service.PersonService;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.BeforeEach;
+import io.quarkus.test.junit.mockito.InjectSpy;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
 @TestHTTPEndpoint(PersonResource.class)
 public class PersonResourceTest {
 
-    PersonRequest person;
-
-    @BeforeEach
-    void setUp() {
-        person = new PersonRequest("Ichwan", "Sholihin", "Natar");
-    }
+    @InjectSpy
+    PersonService personService;
 
     @Test
     void testGetPersonsEndpoint() {
         given().when().get()
                 .then()
                 .statusCode(200);
+
+        Mockito.verify(personService, Mockito.times(1)).getAll();
     }
 
     @Test
     void testGetPersonByIdEndpoint() {
         given()
-                .pathParam("id", 1)
-                .when().get("/{id}")
+                .pathParam("id", 1L)
+                .when().get("{id}")
                 .then()
+                .body("firstname", equalTo("Ichwan"))
                 .statusCode(200);
+
+        Mockito.verify(personService, Mockito.times(1)).getById(1L);
+    }
+
+    @Test
+    void testCreatePersonEndpoint() {
+        given().when().post()
+                .then().statusCode(200);
+
+        Mockito.verify(personService, Mockito.times(1))
+                .create(new PersonRequest("Ahmad", "Abdullah", "Metro"));
     }
 }
--- src/test/java/com/ichwan/books/api/PersonServiceTest.java
+++ src/test/java/com/ichwan/books/api/PersonServiceTest.java
@@ -4,6 +4,7 @@ import com.ichwan.api.entity.Person;
 import com.ichwan.api.service.PersonService;
 import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -16,12 +17,29 @@ public class PersonServiceTest {
     @InjectMock
     PersonService personService;
 
+    Person person;
+
+    @BeforeEach
+    void setUp() {
+        person = new Person(1L,"Test","Test","Test");
+        personService = Mockito.mock(PersonService.class);
+    }
+
     @Test
     void testMockPersonServiceGetAll() {
         List<Person> personList = new ArrayList<>();
-        personList.add(new Person(1L,"Test","Test","Test"));
+        personList.add(person);
 
-        personService = Mockito.mock(PersonService.class);
         Mockito.when(personService.getAll()).thenReturn(personList);
+    }
+
+    @Test
+    void testMockPersonServiceGetById() {
+        Mockito.when(personService.getById(1L)).thenReturn(person);
+    }
+
+    @Test
+    void testMockPersonServiceCreate() {
+
     }
 }
